4caac3a5b80377c7943a12a63470bfd5ade58c30
Dan Lucidi <dalucidi@gmail.com>
2023-11-19 17:49:32 -0500

    Change formatting of help text

--- commands/statsbot.js
+++ commands/statsbot.js
@@ -6,10 +6,10 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(interaction) {
     await interaction.reply('__**---Currently Available Commands---**__\n\
-    **/records {*team name*} |** Gets the current record for a given team\n\
-    **/upcoming {*team name*} |** Gets a teams matchup for the week\n\
-    **/leaders {*stat type*} |** Gets the top 5 players for a given stat\n\
-    **/scoreboard {*team name*} |** Gets the score for a team if they play today\n\
-    **/birds |** birds birds birds');
+    **/records {*team name*}** - Gets the current record for a given team\n\
+    **/upcoming {*team name*}** - Gets a teams matchup for the week\n\
+    **/leaders {*stat type*}** - Gets the top 5 players for a given stat\n\
+    **/scoreboard {*team name*}** - Gets the score for a team if they play today\n\
+    **/birds** - birds birds birds');
 }
 
